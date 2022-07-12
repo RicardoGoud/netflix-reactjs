c5e2e2dc70609ad66225b064216ebcb0c1f77353
RicardoGoud <ricardogoud@gmail.com>
2022-07-12 20:05:24 +0200

    added the navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+        <h1>NETFLIX</h1>
+        <div>
+            <button>Sign In</button>    
+            <button>Sign Up</button>    
+        </div>  
+    </>
+  )
+}
+
+export default Navbar
