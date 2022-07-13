202c41d1e48560ff0cbce5ea5f9487e4713aa986
RicardoGoud <ricardogoud@gmail.com>
2022-07-13 21:15:20 +0200

    completed the navbar and added the TMDB API

---
+++ src/Requests.js
@@ -0,0 +1,10 @@
+const key = 'e859a090013704808d3afd3dadab0104'
+
+const requests = {
+    requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
+    requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
+    requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=2`,
+    requestHorror: `https://api.themoviedb.org/3/search/movie?api_key=${key}&language=en-US&query=horror&page=1`
+}
+
+export default requests
