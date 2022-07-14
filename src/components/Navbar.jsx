940ccc23e9cb8d9f0b88cc5bad8cef577483e9bb
RicardoGoud <ricardogoud@gmail.com>
2022-07-14 16:05:14 +0200

    added the login page and made the route protective

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import {  UserAuth } from '../context/AuthContext'; 
 
 const Navbar = () => {
+  const {user, logOut} = UserAuth()
+  const navigate = useNavigate()
+
+  const handleLogout = async () => {
+    try {
+      await logOut()
+      navigate('/')
+    } catch(error){
+
+    }
+  }
+
   return (
     <div className='flex items-center justify-between p-4 z-[100] w-full absolute'>
       <Link to=''>
@@ -9,6 +22,14 @@ const Navbar = () => {
           NETFLIX
         </h1>
       </Link>
+      {user?.email ?         
+        <div>
+          <Link to='/account'>
+            <button className='text-white pr-4'>Account</button>    
+          </Link>
+            <button onClick={handleLogout} className='bg-red-600 px-6 py-2 rounded cursor-pointer text-white'>Logout</button>    
+        </div>  
+      :         
         <div>
           <Link to='/login'>
             <button className='text-white pr-4'>Sign In</button>    
@@ -17,6 +38,7 @@ const Navbar = () => {
             <button className='bg-red-600 px-6 py-2 rounded cursor-pointer text-white'>Sign Up</button>    
           </Link>
         </div>  
+      }
     </div>
   )
 }
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -6,11 +6,13 @@ const Signup = () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const {user, signUp} = UserAuth()
+    const navigate = useNavigate();
 
     const handleSubmit = async (e) => {
         e.preventDefault()
         try {
             await signUp(email, password)
+            navigate('/')
         } catch (error) {
             console.log(error)
         }
